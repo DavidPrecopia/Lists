7265e50ee64fcbf06b2d4740320742666d82d0df
David <35779726+DavidPrecopia@users.noreply.github.com>
2018-11-05 16:48:54 -0500

    On error for RxJava util

--- app/src/main/java/com/example/david/lists/util/UtilRxJava.java
+++ app/src/main/java/com/example/david/lists/util/UtilRxJava.java
@@ -1,15 +1,35 @@
 package com.example.david.lists.util;
 
 import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import timber.log.Timber;
 
 public final class UtilRxJava {
     private UtilRxJava() {
     }
 
     public static void completableIoAccess(Completable completable) {
-        completable
+        CompletableObserver completableObserver = completable
                 .subscribeOn(Schedulers.io())
-                .subscribe();
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e);
+                    }
+                });
     }
 }
