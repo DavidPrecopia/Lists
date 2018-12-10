19fe03a59608dedc6571e61791607dd42b50529f
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-12-09 19:58:09 -0500

    Have field store default preference value, if night mode pref not found
    More explicit then a literal number.

--- app/src/main/java/com/example/david/lists/util/MyApplication.java
+++ app/src/main/java/com/example/david/lists/util/MyApplication.java
@@ -13,6 +13,9 @@ import androidx.appcompat.app.AppCompatDelegate;
 import timber.log.Timber;
 
 public final class MyApplication extends Application {
+
+    private static final int PREF_NOT_FOUND = -1;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -40,7 +43,7 @@ public final class MyApplication extends Application {
 
     private void setNightMode() {
         switch (getCurrentMode()) {
-            case -1:
+            case PREF_NOT_FOUND:
                 UtilNightMode.setDay();
                 break;
             case AppCompatDelegate.MODE_NIGHT_NO:
@@ -56,7 +59,7 @@ public final class MyApplication extends Application {
 
     private int getCurrentMode() {
         return getSharedPreferences(getString(R.string.night_mode_shared_pref_name), MODE_PRIVATE)
-                .getInt(getString(R.string.night_mode_shared_pref_key), -1);
+                .getInt(getString(R.string.night_mode_shared_pref_key), PREF_NOT_FOUND);
     }
 
 
