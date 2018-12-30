7355bcc348ad966c26bbdef21f6a7b5c4b2daaea
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-12-30 00:32:44 -0500

    Add source set directories and MyApplication class for build type releaseOptimized

---
+++ app/src/releaseOptimized/java/com/example/david/lists/util/MyApplication.java
@@ -0,0 +1,58 @@
+package com.example.david.lists.util;
+
+import android.app.Application;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+import com.crashlytics.android.core.CrashlyticsCore;
+import com.example.david.lists.R;
+
+import androidx.appcompat.app.AppCompatDelegate;
+import io.fabric.sdk.android.Fabric;
+
+public final class MyApplication extends Application {
+
+    private static final int PREF_NOT_FOUND = -1;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        checkNetworkConnection();
+        setNightMode();
+        initFabric();
+    }
+
+    private void checkNetworkConnection() {
+        NetworkInfo networkInfo = ((ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE))
+                .getActiveNetworkInfo();
+        if (networkInfo == null) {
+            Toast.makeText(getApplicationContext(), R.string.error_msg_no_network_connection, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void setNightMode() {
+        switch (getCurrentMode()) {
+            case PREF_NOT_FOUND:
+                UtilNightMode.setDay();
+                break;
+            case AppCompatDelegate.MODE_NIGHT_NO:
+                UtilNightMode.setDay();
+                break;
+            case AppCompatDelegate.MODE_NIGHT_YES:
+                UtilNightMode.setNight();
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private int getCurrentMode() {
+        return getSharedPreferences(getString(R.string.night_mode_shared_pref_name), MODE_PRIVATE)
+                .getInt(getString(R.string.night_mode_shared_pref_key), PREF_NOT_FOUND);
+    }
+
+    private void initFabric() {
+        Fabric.with(this, new CrashlyticsCore());
+    }
+}
