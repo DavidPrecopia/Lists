461631d3c9b0d073e2bbbddcad4d8ff1991ffbd3
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-02-07 22:51:46 -0500

    Test UtilNightMode

---
+++ app/src/androidTest/java/com/example/david/lists/util/UtilNightModeTest.java
@@ -0,0 +1,28 @@
+package com.example.david.lists.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class UtilNightModeTest {
+    @Test
+    public void setDayTest() {
+        UtilNightMode.setDay();
+        assertEquals(AppCompatDelegate.MODE_NIGHT_NO, getCurrentNightModeStatus());
+    }
+
+    @Test
+    public void setNightTest() {
+        UtilNightMode.setNight();
+        assertEquals(AppCompatDelegate.MODE_NIGHT_YES, getCurrentNightModeStatus());
+    }
+
+    private int getCurrentNightModeStatus() {
+        return AppCompatDelegate.getDefaultNightMode();
+    }
+}
