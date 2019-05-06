68ebec3e4ec21f02fee10c502b017316d8a5e90b
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-05-06 18:41:29 -0400

    Update LeakCanary
    v1.6.3 -> v2.0-alpha-1

--- app/src/debug/java/com/example/david/lists/application/InitDebug.java
+++ app/src/debug/java/com/example/david/lists/application/InitDebug.java
@@ -3,7 +3,6 @@ package com.example.david.lists.application;
 import android.app.Application;
 
 import com.example.david.lists.di.data.AppComponent;
-import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
 
@@ -18,20 +17,10 @@ final class InitDebug {
     public AppComponent init() {
         AppComponent appComponent = new InitRelease(application).init();
         initTimber();
-        initLeakCanary();
         return appComponent;
     }
 
     private void initTimber() {
         Timber.plant(new Timber.DebugTree());
-    }
-
-    private void initLeakCanary() {
-        if (LeakCanary.isInAnalyzerProcess(application)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in context process.
-            return;
-        }
-        LeakCanary.install(application);
     }
 }
