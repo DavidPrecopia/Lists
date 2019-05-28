d8f832306aa85947b5e061039b3fb273a04422ee
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-05-28 14:03:48 -0400

    ViewCommonModule: removed scope from some provider methods
    AppComponent maintains Singletons for this dependencies, thus there is
    no reason to cache them here.

--- app/src/main/java/com/example/david/lists/di/view/common/ViewCommonModule.java
+++ app/src/main/java/com/example/david/lists/di/view/common/ViewCommonModule.java
@@ -10,30 +10,25 @@ import com.example.david.lists.di.data.AppComponent;
 
 import dagger.Module;
 import dagger.Provides;
-import dagger.Reusable;
 import io.reactivex.disposables.CompositeDisposable;
 
 @Module
 public final class ViewCommonModule {
-    @Reusable
     @Provides
     IRepository repository(AppComponent appComponent) {
         return appComponent.repository();
     }
 
-    @Reusable
     @Provides
     IUserRepository userRepository(AppComponent appComponent) {
         return appComponent.userRepository();
     }
 
-    @Reusable
     @Provides
     SharedPreferences sharedPreferences(AppComponent appComponent) {
         return appComponent.sharedPrefsNightMode();
     }
 
-    @Reusable
     @Provides
     AppComponent appComponent(Application application) {
         return ((ListsApplicationImpl) application).getAppComponent();
