d16c7cf47930302313d9ccf42b2821718e1709b1
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-09-29 16:51:35 -0400

    Updated UtilNightModeTest

--- app/src/androidTest/java/com/example/david/lists/util/UtilNightModeTest.kt
+++ app/src/androidTest/java/com/example/david/lists/util/UtilNightModeTest.kt
@@ -2,6 +2,7 @@ package com.example.david.lists.util
 
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.david.lists.common.ListsApplication
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.junit.Test
@@ -10,9 +11,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class UtilNightModeTest {
 
-    private val utilNightMode = UtilNightMode(
-            ApplicationProvider.getApplicationContext()
-    )
+    private val utilNightMode = (ApplicationProvider.getApplicationContext() as ListsApplication)
+            .appComponent.utilNightMode()
 
     @Test
     fun setDayTest() {
