8fbebc5520394e69943b89af5397633632563ed4
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-10-27 17:37:17 -0400

    Refactored const vals to top-level declaration

--- app/src/main/java/com/example/david/lists/widget/view/buildlogic/MyRemoteViewsServiceModule.kt
+++ app/src/main/java/com/example/david/lists/widget/view/buildlogic/MyRemoteViewsServiceModule.kt
@@ -15,14 +15,12 @@ import dagger.Provides
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Named
 
+private const val PACKAGE_NAME = "package_name"
+private const val USER_LIST_ID = "user_list_id"
+private const val APP_WIDGET_ID = "app_widget_id"
+
 @Module
 class MyRemoteViewsServiceModule {
-    companion object {
-        private const val PACKAGE_NAME = "package_name"
-        private const val USER_LIST_ID = "user_list_id"
-        private const val APP_WIDGET_ID = "app_widget_id"
-    }
-
     @ViewScope
     @Provides
     fun remoteViewsFactory(@Named(PACKAGE_NAME) packageName: String,
