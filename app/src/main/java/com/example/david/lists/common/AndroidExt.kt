3464fb809d1b0d3d2d2e619dce4c05d75d7c7094
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-11-18 10:24:01 -0500

    Added Navigation extension functions to Fragment

--- app/src/main/java/com/example/david/lists/common/AndroidExt.kt
+++ app/src/main/java/com/example/david/lists/common/AndroidExt.kt
@@ -2,9 +2,19 @@ package com.example.david.lists.common
 
 import android.app.Application
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 import org.jetbrains.anko.toast
 
 val Fragment.application: Application
     get() = activity!!.application
 
 fun Fragment.toast(message: String) = context!!.toast(message)
+
+fun Fragment.navigate(direction: NavDirections) {
+    findNavController().navigate(direction)
+}
+
+fun Fragment.navigateUp() {
+    findNavController().navigateUp()
+}
