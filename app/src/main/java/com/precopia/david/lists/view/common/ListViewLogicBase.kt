e78aa04acf26e61f50ad23745c2e2ec04ed98323
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2020-06-06 18:42:53 -0400

    Refactored ListViewLogicBase to extend AndroidX ViewModel

--- app/src/main/java/com/precopia/david/lists/view/common/ListViewLogicBase.kt
+++ app/src/main/java/com/precopia/david/lists/view/common/ListViewLogicBase.kt
@@ -1,9 +1,10 @@
 package com.precopia.david.lists.view.common
 
+import androidx.lifecycle.ViewModel
 import com.precopia.david.lists.util.ISchedulerProviderContract
 import com.precopia.domain.repository.IRepositoryContract
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 abstract class ListViewLogicBase protected constructor(protected val repo: IRepositoryContract.Repository,
                                                        protected val schedulerProvider: ISchedulerProviderContract,
-                                                       protected val disposable: CompositeDisposable)
+                                                       protected val disposable: CompositeDisposable): ViewModel()
